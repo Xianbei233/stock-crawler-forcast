0b00f219c43d5f7fbf62b200f349f949dd9683ab
Xianbei233 <455482921@qq.com>
2019-05-19 19:59:29 +0800

    修复无csv文件夹问题

--- db/index.js
+++ db/index.js
@@ -3,12 +3,13 @@ const config = require('../config')
 const { exec } = require('child_process');
 const utils = require('util')
 const path = require('path')
+const fs = require('fs')
 const formatter = require('../csvPaser')
 const newExec = utils.promisify(exec)
 const client = redis.createClient(config.redis.port, config.redis.host)
 
 const db = {}
-
+let hasCSVfolder = fs.existsSync(path.resolve(__dirname, '../csv'))
 client.auth(config.redis.auth, function () {
     console.log('认证通过')
 })
@@ -34,23 +35,11 @@ db.setStock = (id, date, highest, lowest, open, close, volume) => {
     })
 }
 
-
-db.getStock = id => {
-    return new Promise((resolve, reject) => {
-        client.hgetall(id, (err, res) => {
-            if (err) {
-                reject(err)
-            }
-            if (!res) {
-                reject('There is no result')
-            }
-            res = JSON.parse(res)
-            resolve(res)
-        })
-    })
-}
-
 db.getStockCSV = async id => {
+    if (!hasCSVfolder) {
+        fs.mkdirSync(path.resolve(__dirname, '../csv'))
+    }
+
     try {
         await newExec(`redis-cli -a ${config.redis.auth} --csv hgetall ${id} > ${path.resolve(__dirname, '../csv')}/${id}.csv 2> stderr.txt`)
         formatter(id)
